66af4ff1f9db274ab78100528824538258159c98
Semih Büyükboz <bykbzsemih81@gmail.com>
2022-06-29 13:21:04 +0300

    Anket Projesi

---
+++ AnketProjesi/AnketProjesi/Controllers/AnketOlusturController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AnketProjesi.Models.Entity;
+
+namespace AnketProjesi.Controllers
+{
+    public class AnketOlusturController : Controller
+    {
+        AnketProjesiDBEntities db = new AnketProjesiDBEntities();
+        // GET: AnketOlustur
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ANKT_Anketler p1,ANKT_Sorular p2,ANKT_Secenekler p3)
+        {
+            db.ANKT_Anketler.Add(p1);
+            db.ANKT_Sorular.Add(p2);
+            db.ANKT_Secenekler.Add(p3);
+            db.SaveChanges();
+            return View(p1);
+        }
+    }
+}
